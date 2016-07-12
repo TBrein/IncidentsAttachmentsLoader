1d5912cc3eb35ca6e8b237d8855a1ca8188426dc
TBrein <igneuslacerta@gmail.com>
2016-07-13 01:36:00 +0600

    Обширные изменения.

--- src/assyst/attachments/Controller.java
+++ src/assyst/attachments/Controller.java
@@ -1,34 +1,54 @@
+/*
+ * Данная программа разработана Бедаревым Вячеславом специально для аналитиков L3 проекта МВИДЕО "ФОБО".
+ *
+ * Использование вне проекта запрещено!
+ */
+
 package assyst.attachments;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 import javafx.application.Platform;
-import javafx.scene.control.Button;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Pair;
 
-import javax.swing.JOptionPane;
 import java.io.*;
 import java.sql.*;
+import java.util.Optional;
 
 public class Controller {
 
     public Button downloadButton;
     public TextField incidentNumber;
     public ProgressBar downloadBar;
-    private Connection con;
+    public Label statusLabel;
     private String login = "";
     private String password = "";
+    private String saveDir = "C:\\TMP\\Download\\";
 
     public void downloadAction() {
         final int[] fc = new int[1];
-        if (login.isEmpty()) {
-            login = JOptionPane.showInputDialog(null, "Введите логин от ASSYST", "Введите логин", JOptionPane.INFORMATION_MESSAGE);
-        }
-        if (password.isEmpty()) {
-            password = JOptionPane.showInputDialog(null, "Введите пароль от ASSYST", "Введите пароль", JOptionPane.INFORMATION_MESSAGE);
-        }
+        Alert viewError = new Alert(Alert.AlertType.ERROR);
+        Alert viewInfo = new Alert(Alert.AlertType.INFORMATION);
+        Stage viewErrorStage = (Stage) viewError.getDialogPane().getScene().getWindow();
+        viewErrorStage.setAlwaysOnTop(true);
+        Stage viewInfoStage = (Stage) viewInfo.getDialogPane().getScene().getWindow();
+        viewInfoStage.setAlwaysOnTop(true);
+
+        viewError.initModality(Modality.APPLICATION_MODAL);
+        viewInfo.initModality(Modality.APPLICATION_MODAL);
+
+        viewErrorStage.getIcons().add(new Image(this.getClass().getResource("res/icon16.png").toString()));
+        viewInfoStage.getIcons().add(new Image(this.getClass().getResource("res/icon16.png").toString()));
+
         if (incidentNumber.getText().length() > 0) {
             int b;
             try {
@@ -36,22 +56,47 @@ public class Controller {
             } catch (NumberFormatException e) {
                 b = -1;
             }
-            if ((incidentNumber.getText().length() > 6) && (b > 1000000)) {
+            if ((incidentNumber.getText().length() == 7) && (b > 1000000)) {
+                if (login.isEmpty() | password.isEmpty()) getLoginAndPassword();
+
                 downloadButton.setDisable(true);
                 incidentNumber.setDisable(true);
                 try {
-                    if (checkIncidentNumber(incidentNumber.getText())) {
+                    int res = checkIncidentNumber(incidentNumber.getText());
+                    if (res > 0) {
+                        //todo Доделать окно выбора папки
+/*                        DirectoryChooser directoryChooser = new DirectoryChooser();
+                        directoryChooser.setInitialDirectory(new File(saveDir));
+                        directoryChooser.setTitle("Выберите папку для сохранения выгружаемых вложений");
+                        File selectedDirectory = directoryChooser.showDialog(null);
+                        System.out.println(selectedDirectory.getAbsolutePath());
+*/
                         downloadBar.setVisible(true);
                         new Thread(() -> {
                             try {
                                 fc[0] = saveBLOBdataToFiles(incidentNumber.getText());
                                 if (fc[0] > 0) {
-                                    JOptionPane.showMessageDialog(null, "Из инцидента " + incidentNumber.getText() + " сохранено " + fc[0] + " файла/ов.", "Успешное сохранение", JOptionPane.INFORMATION_MESSAGE);
-                                } else
-                                    JOptionPane.showMessageDialog(null, "В инциденте " + incidentNumber.getText() + " не найдено файлов!", "Ошибка при сохранении", JOptionPane.ERROR_MESSAGE);
-
+                                    Platform.runLater(() -> {
+                                        viewInfo.setTitle("Успешное сохранение");
+                                        viewInfo.setHeaderText(null);
+                                        viewInfo.setContentText("Из инцидента " + incidentNumber.getText() + " выгружено " + fc[0] + " вложений.");
+                                        viewInfo.showAndWait();
+                                    });
+                                } else {
+                                    Platform.runLater(() -> {
+                                        viewError.setTitle("Ошибка при сохранении");
+                                        viewError.setHeaderText(null);
+                                        viewError.setContentText("В инциденте " + incidentNumber.getText() + " нет ни одного вложения!");
+                                        viewError.showAndWait();
+                                    });
+                                }
                             } catch (SQLException | FileNotFoundException e) {
-                                JOptionPane.showMessageDialog(null, "Ай, яй! Ошибочка вышла! (" + e.getLocalizedMessage() + ")", "Ошибка при выполнении", JOptionPane.ERROR_MESSAGE);
+                                Platform.runLater(() -> {
+                                    viewError.setTitle("Ошибка при выполнении");
+                                    viewError.setHeaderText("Ай, яй! Ошибочка вышла! Сообщите о ней на vbedarev@luxoft.com. Спасибо.");
+                                    viewError.setContentText(e.getLocalizedMessage());
+                                    viewError.showAndWait();
+                                });
                             }
                             Platform.runLater(() -> {
                                 downloadBar.setVisible(false);
@@ -60,35 +105,124 @@ public class Controller {
                                 incidentNumber.setDisable(false);
                             });
                         }).start();
+                    }
+                    if ((res != -1) && (res == 0)) {
+                        downloadButton.setDisable(false);
+                        incidentNumber.setDisable(false);
+                        viewError.setTitle("Ошибка");
+                        viewError.setHeaderText("Похоже, Вы ввели некорректный номер инцидента!");
+                        viewError.setContentText("Проверьте правильность ввода номера инцидента. Не вводите преффиксы (T, R и др.). Номер инцидента должен содержать 7 цифр.\nПример: 2046188 или 2041272");
+                        viewError.showAndWait();
                     } else {
-                        downloadButton.setDisable(true);
-                        incidentNumber.setDisable(true);
-                        JOptionPane.showMessageDialog(null, "Похоже Вы ввели некорректный номер инцидента!", "Ошибка", JOptionPane.ERROR_MESSAGE);
+                        downloadButton.setDisable(false);
+                        incidentNumber.setDisable(false);
                     }
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-            } else
-                JOptionPane.showMessageDialog(null, "Похоже Вы ввели некорректный номер инцидента!", "Ошибка", JOptionPane.ERROR_MESSAGE);
-        } else
-            JOptionPane.showMessageDialog(null, "Введите номер инцидента! (Например, 2046188 или T2041272)", "Ошибка", JOptionPane.ERROR_MESSAGE);
-    }
-
-    public void checkInput(KeyEvent event) {
-/*        if (event.getEventType() == KEY_TYPED) {
-            if (Objects.equals("T", event.getCharacter())) {
-
+            } else {
+                viewError.setTitle("Ошибка");
+                viewError.setHeaderText("Похоже, Вы ввели некорректный номер инцидента!");
+                viewError.setContentText("Проверьте правильность ввода номера инцидента. Не вводите преффиксы (T, R и др.). Номер инцидента должен содержать 7 цифр.\nПример: 2046188 или 2041272");
+                viewError.showAndWait();
             }
-            System.out.println("Key typed: " + incidentNumber.getText());
+        } else {
+            viewError.setTitle("Ошибка");
+            viewError.setHeaderText("Введите номер инцидента!");
+            viewError.setContentText("Не вводите преффиксы (T, R и др.). Номер инцидента должен содержать 7 цифр.\nПример, 2046188 или 2041272");
+            viewError.showAndWait();
         }
-*/
     }
 
-    private boolean checkIncidentNumber(String incident) throws SQLException {
+    private boolean getLoginAndPassword() {
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Аутентификация БД АССИСТ");
+        dialog.setHeaderText("Пройдите аутентификацию");
+
+        Stage stage = (Stage) dialog.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(this.getClass().getResource("res/icon16.png").toString()));
+        stage.setAlwaysOnTop(true);
+
+        dialog.initModality(Modality.APPLICATION_MODAL);
+
+        dialog.setGraphic(new ImageView(this.getClass().getResource("res/login.png").toString()));
+
+        ButtonType loginButtonType = new ButtonType("Войти", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField username = new TextField();
+        username.setPromptText("Введите логин");
+        PasswordField pass = new PasswordField();
+        pass.setPromptText("Введите пароль");
+
+        grid.add(new Label("Логин:"), 0, 0);
+        grid.add(username, 1, 0);
+        grid.add(new Label("Пароль:"), 0, 1);
+        grid.add(pass, 1, 1);
+
+        Node loginButton = dialog.getDialogPane().lookupButton(loginButtonType);
+        loginButton.setDisable(true);
+
+        pass.textProperty().addListener((observable, oldValue, newValue) -> {
+            loginButton.setDisable(oldValue.trim().isEmpty());
+        });
+
+        dialog.getDialogPane().setContent(grid);
+
+        // устанавливаем фокус в поле username
+        Platform.runLater(username::requestFocus);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == loginButtonType) {
+                return new Pair<>(username.getText(), pass.getText());
+            }
+            return null;
+        });
+
+        Optional<Pair<String, String>> result = dialog.showAndWait();
+
+        result.ifPresent(usernamePassword -> {
+            login = usernamePassword.getKey();
+            password = usernamePassword.getValue();
+        });
+        return false;
+    }
+
+    private int checkIncidentNumber(String incident) throws SQLException {
+        Connection con = null;
+        ResultSet rs = null;
         String sqlQuery = "SELECT count(*) AS \"count\" FROM incident WHERE incident_ref = %s;";
         sqlQuery = String.format(sqlQuery, incident);
-        ResultSet rs = executeSQLQuery(getDBConnection(login, password), sqlQuery);
-        return rs != null && rs.next() && (Integer.parseInt(rs.getString("count")) > 0);
+        con = getDBConnection(login, password);
+        if (con != null) {
+            rs = executeSQLQuery(con, sqlQuery);
+            if (rs != null && rs.next()) return Integer.parseInt(rs.getString("count"));
+            else return 0;
+        }
+        return -1;
+    }
+
+    private String getAssystUserNameByLogin(Connection con, String login) {
+        ResultSet rs;
+        if (con != null) {
+            rs = executeSQLQuery(con, String.format("SELECT u.assyst_usr_n FROM assyst_usr u WHERE u.assyst_usr_sc = '%s';", login));
+            try {
+                if (rs != null && rs.next()) {
+                    return rs.getString("assyst_usr_n");
+                } else {
+                    System.out.println("Ошибка!");
+                }
+            } catch (SQLException e) {
+                System.out.println("Ошибка!");
+            }
+        }
+        return "";
     }
 
     private int saveBLOBdataToFiles(String incident) throws SQLException, FileNotFoundException {
@@ -101,9 +235,9 @@ public class Controller {
             try {
                 Blob blob = rs.getBlob("ole_item");
                 InputStream inputStream = blob.getBinaryStream();
-                File myPath = new File("C:\\TMP\\Download\\" + incident + "\\");
+                File myPath = new File(saveDir + incident + "\\");
                 myPath.mkdirs();
-                OutputStream outputStream = new FileOutputStream("C:\\TMP\\Download\\" + incident + "\\" + rs.getString("ole_filename"));
+                OutputStream outputStream = new FileOutputStream(saveDir + incident + "\\" + rs.getString("ole_filename"));
 
                 int bytesRead;
                 byte[] buffer = new byte[1024];
@@ -123,6 +257,7 @@ public class Controller {
     }
 
     private Connection getDBConnection(String login, String pass) {
+        Connection con;
         SQLServerDataSource ds = new SQLServerDataSource();
         ds.setServerName("10.95.1.56");
         ds.setPortNumber(1433);
@@ -130,9 +265,26 @@ public class Controller {
         ds.setUser(login);
         ds.setPassword(pass);
         try {
-            return ds.getConnection();
+            if (!login.isEmpty() && !pass.isEmpty()) {
+                con = ds.getConnection();
+                statusLabel.setText("Добро пожаловать, " + getAssystUserNameByLogin(con, login));
+                return con;
+            }
         } catch (SQLServerException e) {
-            JOptionPane.showMessageDialog(null, "Не удалось подключиться к БД ASSYST под логином \"" + login + "\"", "Ошибка подключения", JOptionPane.ERROR_MESSAGE);
+            this.login = "";
+            this.password = "";
+
+            Alert viewError = new Alert(Alert.AlertType.ERROR);
+
+            Stage viewErrorStage = (Stage) viewError.getDialogPane().getScene().getWindow();
+            viewErrorStage.getIcons().add(new Image(this.getClass().getResource("res/icon16.png").toString()));
+            viewErrorStage.setAlwaysOnTop(true);
+            viewError.initModality(Modality.APPLICATION_MODAL);
+
+            viewError.setTitle("Ошибка аутентификации");
+            viewError.setHeaderText(null);
+            viewError.setContentText("Произошла ошибка аутентификации при использовании логина \"" + login + "\".\nПроверьте верность ввода логина, пароля и попробуйте снова.");
+            viewError.showAndWait();
         }
         return null;
     }
--- src/assyst/attachments/Main.java
+++ src/assyst/attachments/Main.java
@@ -1,9 +1,16 @@
+/*
+ * Данная программа разработана Бедаревым Вячеславом специально для аналитиков L3 проекта МВИДЕО "ФОБО".
+ *
+ * Использование вне проекта запрещено!
+ */
+
 package assyst.attachments;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -12,14 +19,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("res/LoadIncidentAttachments.fxml"));
-        primaryStage.setTitle("Assyst Incident Attachments Loader (version 0.1)");
-        primaryStage.setScene(new Scene(root, 300, 90));
+        primaryStage.setTitle("Incident Attach's Loader");
+        primaryStage.setScene(new Scene(root, 300, 100));
         primaryStage.setAlwaysOnTop(true);
-        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.getIcons().add(new Image(this.getClass().getResource("res/icon32.png").toString()));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
